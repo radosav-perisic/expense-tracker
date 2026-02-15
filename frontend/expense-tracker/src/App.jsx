23a23d66f3d781d49ce7e690ccda2428af58fb7d
Radosav <radosavperisic.contact@gmail.com>
2026-02-15 13:08:09 +0100

    Initial commit

---
+++ frontend/expense-tracker/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+function App() {
+
+  return (
+    <>
+      <p className="text-amber-300">App </p>
+    </>
+  );
+}
+
+export default App;
